b1d39e859d692e3e45504f508bed79003101affa
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2021-03-15 20:13:20 -0300

    Added things
    Dirt sprites added.
    A few characters components are modified.

--- Assets/Code/Player/PlayerController.cs
+++ Assets/Code/Player/PlayerController.cs
@@ -8,9 +8,7 @@ public sealed class PlayerController : MonoBehaviour
 {
     #region Properties
     [Header("Movement settings")]
-    [Tooltip("Jump force to apply when jumping, not multiplied by Time.deltaTime, so for better results its recommended use low values.")]
-    [SerializeField] float jumpForce;
-    [Tooltip("Speed ammount to apply when player moves on horizontal, multiplied by Time.deltaTime.")]
+    [Tooltip("Speed ammount to apply when player moves, multiplied by Time.deltaTime.")]
     [SerializeField] float speed;
     [Tooltip("The animator of the complete back arm.")]
     [SerializeField]Animator backArmAnimator;
@@ -37,13 +35,11 @@ public sealed class PlayerController : MonoBehaviour
     private Vector2 mouseSightPosition;
     public Transform secondHandGrab{get;set;}
     public Transform twoHandsGun{get;set;}
-    private bool jump, idle, isActivated, movement = true, crouch, onGround, death;
+    private bool idle, movement = true, onGround, death;
     public bool IsDeath { set => death = value; }
     public bool Movement { set => movement = value; }
     public bool IsGrounded { set => onGround = value; }
-    private float xInput;
-    private const float timeJumping = .25f;
-    public bool IsActivated { get => isActivated; }
+    private float xInput,yInput;
     #endregion
     #region Unity Methods
     void Awake()
@@ -58,12 +54,12 @@ public sealed class PlayerController : MonoBehaviour
         if (movement)
         {
             float xVelo = speed * xInput * Time.deltaTime;
-            if (jump) rigid.SetVelocity(xVelo, jumpForce);
-            else rigid.SetVelocity(xVelo, rigid.velocity.y);
+            float yVelo=speed*yInput*Time.deltaTime;
+            rigid.SetVelocity(xVelo, yVelo);
         }
         else if (death)
         {
-            rigid.SetVelocity(0, rigid.velocity.y);
+            rigid.SetVelocity(0, 0);
         }
     }
     private void Update()
@@ -79,8 +75,6 @@ public sealed class PlayerController : MonoBehaviour
         animator.SetBool("grounded", onGround);
         backArmAnimator.SetBool("grounded",onGround);
 
-        animator.SetBool("onJump",jump);
-        backArmAnimator.SetBool("onJump", jump);
 
         if(onGround){
             animator.SetBool("walk", xInput != 0);
@@ -105,17 +99,6 @@ public sealed class PlayerController : MonoBehaviour
     /// </summary>
     private void OnAir()
     {
-        CheckHeadCollisionOnJumping();
-    }
-    /// <summary>
-    /// Checks if player is colliding with ground over him.
-    /// </summary>
-    private void CheckHeadCollisionOnJumping()
-    {
-        if (Physics2D.Raycast(transform.position, Vector2.up, distanceTop, groundLayer))
-        {
-            jump = false;
-        }
     }
     #endregion
     #region On ground methods
@@ -151,23 +134,14 @@ public sealed class PlayerController : MonoBehaviour
         if (context.performed && movement)
         {
             xInput = context.ReadValue<Vector2>().x;
+            yInput=context.ReadValue<Vector2>().y;
             if (xInput > 0) transform.eulerAngles.Set(0, 0, 0);
             else transform.eulerAngles.Set(0, 180, 0);
         }
-        else
-        {
-            xInput = 0;
+        else{
+            if(yInput!=0)yInput=0;
+            if(xInput!=0)xInput=0;
         }
-    }
-    public void OnJump(InputAction.CallbackContext context)
-    {
-        if (context.performed && movement && onGround)
-        {
-            jump = true;
-            animator.SetTrigger("jump");
-            StartCoroutine(Jumping());
-        }
-        else jump = false;
     }
     public void OnFire(InputAction.CallbackContext context){
         if(context.performed && movement){
@@ -177,19 +151,4 @@ public sealed class PlayerController : MonoBehaviour
         }
     }
     #endregion
-    /// <summary>
-    /// Increase player jump altitude over time, when held the jump button.
-    /// </summary>
-    /// <returns></returns>
-    IEnumerator Jumping()
-    {
-        float currentTimeJumping = 0;
-        while (currentTimeJumping <= timeJumping && jump)
-        {
-            currentTimeJumping += 0.01f;
-            yield return new WaitForSecondsRealtime(0.005f);
-        }
-        rigid.SetVelocity(rigid.velocity.x,0);
-        jump = false; 
-    }
 }

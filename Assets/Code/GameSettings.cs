e586a7eee074b10ad7604aed7a4399a4288608fd
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2021-03-21 17:41:52 -0300

    Removed scene transition, game is too faster for that.

--- Assets/Code/GameSettings.cs
+++ Assets/Code/GameSettings.cs
@@ -8,8 +8,6 @@ public class GameSettings : MonoBehaviour
 {
     [SerializeField] GameObject redCrossMute;
     [SerializeField] AudioMixer soundsEffects;
-    [SerializeField]Animator sceneTransition;
-    [SerializeField] TextMeshProUGUI percentajeText,loadingText;
     public void MuteAll(){
         bool muted = AudioListener.pause = !AudioListener.pause;
         if (muted) redCrossMute.SetActive(true);
@@ -23,27 +21,6 @@ public class GameSettings : MonoBehaviour
         Application.Quit();
     }
     public void LoadScene(int index){
-        sceneTransition.enabled=true;
-        StartCoroutine(CheckSceneLoaded(index));
-    }
-    IEnumerator CheckSceneLoaded(int index){
-        yield return null;
-        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
-        loadingText.text="Loading";
-        asyncOperation.allowSceneActivation = false;
-        while (!asyncOperation.isDone)
-        {
-            percentajeText.text = (asyncOperation.progress * 100) + "%";
-
-
-            // Check if the load has finished
-            if (asyncOperation.progress >= 0.9f)
-            {
-                asyncOperation.allowSceneActivation = true;
-                sceneTransition.SetTrigger("Loaded");
-            }
-
-            yield return null;
-        }
+        SceneManager.LoadSceneAsync(index);
     }
 }

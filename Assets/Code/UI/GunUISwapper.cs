796bd75fbe56d5769c5f179cf64b2b810a3d54a4
Sebastian Cataldo Smith <sebacataldos@gmail.com>
2021-03-21 21:25:23 -0300

    Added code

---
+++ Assets/Code/UI/GunUISwapper.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunUISwapper : MonoBehaviour
+{
+    [SerializeField]RectTransform gunBullets;
+    [SerializeField]int gunBulletSizeWith;
+    public static Action<int> gunSwapper;
+    private void OnEnable() {
+        gunSwapper+=HandleUISwap;
+    }
+    private void HandleUISwap(int index){
+        gunBullets.sizeDelta=new Vector2(gunBullets.sizeDelta.x,gunBullets.sizeDelta.y-gunBulletSizeWith);
+    }
+    private void OnDisable() {
+        gunSwapper-=HandleUISwap;
+    }
+}
